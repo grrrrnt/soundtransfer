7f0fe4b5e052d25887bddbe63f25678a0f374ec2
grrrrnt <grantlee97@gmail.com>
2023-12-08 19:28:51 -0500

    Template dashboard

--- web/visualizer/src/App.js
+++ web/visualizer/src/App.js
@@ -1,24 +1,403 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import PeopleIcon from "@mui/icons-material/People";
+import AlbumIcon from "@mui/icons-material/Album";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import HistoryIcon from "@mui/icons-material/History";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import InputIcon from "@mui/icons-material/Input";
+import OutputIcon from "@mui/icons-material/Output";
+
+const defaultTheme = createTheme();
+const drawerWidth = 240;
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      Copyright © Abhyudaya Sharma, Grant Lee, Muskaan Patel (Brown University){" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
 
 function App() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar sx={{ pr: "24px" }}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Music Streaming Adapter
+            </Typography>
+            <IconButton color="inherit"></IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            <ListItemButton>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+            <Divider sx={{ my: 1 }} />
+            <ListSubheader component="div" inset>
+              Data Types
+            </ListSubheader>
+            <ListItemButton>
+              <ListItemIcon>
+                <MusicNoteIcon />
+              </ListItemIcon>
+              <ListItemText primary="Saved Songs" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Followed Artists" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <AlbumIcon />
+              </ListItemIcon>
+              <ListItemText primary="Saved Albums" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <PlaylistPlayIcon />
+              </ListItemIcon>
+              <ListItemText primary="Playlists" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Listening History" />
+            </ListItemButton>
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar />
+          <Container
+            maxWidth="xl"
+            sx={{ mt: 4, mb: 4 }}
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  You've saved
+                </Typography>
+                <Typography component="p" variant="h4">
+                  100
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  songs
+                </Typography>
+              </Paper>
+            </Container>
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  You've saved
+                </Typography>
+                <Typography component="p" variant="h4">
+                  100
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  albums
+                </Typography>
+              </Paper>
+            </Container>
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  You've followed
+                </Typography>
+                <Typography component="p" variant="h4">
+                  100
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  artists
+                </Typography>
+              </Paper>
+            </Container>
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  You have
+                </Typography>
+                <Typography component="p" variant="h4">
+                  100
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  playlists
+                </Typography>
+              </Paper>
+            </Container>
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  Your listening history has
+                </Typography>
+                <Typography component="p" variant="h4">
+                  100
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  records
+                </Typography>
+              </Paper>
+            </Container>
+          </Container>
+          <Container
+            maxWidth="md"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  width: "100%",
+                  alignItems: "start",
+                }}
+              >
+                <Typography color="text.secondary">Spotify actions</Typography>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <InputIcon />
+                  <Typography>Ingest from data export file</Typography>
+                </IconButton>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <InputIcon />
+                  <Typography>Ingest via API</Typography>
+                </IconButton>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <OutputIcon />
+                  <Typography>Export via API</Typography>
+                </IconButton>
+              </Paper>
+            </Container>
+            <Container style={{ flex: "1" }}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  width: "100%",
+                  alignItems: "start",
+                }}
+              >
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                  Apple Music actions
+                </Typography>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <InputIcon />
+                  <Typography>Ingest from data export file</Typography>
+                </IconButton>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <InputIcon />
+                  <Typography>Ingest via API</Typography>
+                </IconButton>
+
+                <IconButton
+                  className="action-button"
+                  color="inherit"
+                  onClick={toggleDrawer}
+                >
+                  <OutputIcon />
+                  <Typography>Export via API</Typography>
+                </IconButton>
+              </Paper>
+            </Container>
+          </Container>
+          <Container>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- web/visualizer/src/index.js
+++ web/visualizer/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
