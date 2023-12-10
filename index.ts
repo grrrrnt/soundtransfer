bca8bb664f27515694a24aa4c8c16caf034579bd
Abhyudaya Sharma <11471599+AbhyudayaSharma@users.noreply.github.com>
2023-12-10 15:39:18 -0500

    Print out url to visit when starting express

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { listen } from './web/express';
+import { listen, port } from './web/express';
 import { connectDB } from './lib/mongo';
 
 dotenv.config();
@@ -7,6 +7,7 @@ dotenv.config();
 async function main() {
   await connectDB();
   await listen();
+  console.log(`Please visit http://localhost:${port}`);
 }
 
 main()
